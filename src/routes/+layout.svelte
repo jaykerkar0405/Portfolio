452b5f93006a2528ba8ec7997c7d50aaf33ec637
Jay Kerkar <bytewise0405@gmail.com>
2025-12-21 13:54:17 +0530

    feat: Refactor layout structure to include ModeWatcher and improve component organization

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
 	import './layout.css';
+	import { ModeWatcher } from 'mode-watcher';
+	import Header from '$lib/components/header.svelte';
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+<ModeWatcher />
+
+<div class="relative flex min-h-screen flex-col">
+	<Header />
+
+	<main class="flex-1">
+		{@render children()}
+	</main>
+</div>
