a4acdef48084637d66af8268fea0b41533eeda8b
Jay Kerkar <bytewise0405@gmail.com>
2025-12-21 13:53:45 +0530

    feat: Add button component with variants and props for enhanced UI flexibility

---
+++ src/lib/components/ui/button/index.ts
@@ -0,0 +1,11 @@
+import Root, { buttonVariants, type ButtonSize, type ButtonProps, type ButtonVariant } from './button.svelte';
+
+export {
+	Root,
+	Root as Button,
+	buttonVariants,
+	type ButtonSize,
+	type ButtonProps,
+	type ButtonVariant,
+	type ButtonProps as Props
+};
