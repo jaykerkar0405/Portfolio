75728b4ea778882328424491c2509fa32d0af00c
Jay Kerkar <bytewise0405@gmail.com>
2025-12-21 16:45:33 +0530

    feat: Implement card component structure with title, footer, header, action, content, and description

---
+++ src/lib/components/ui/card/index.ts
@@ -0,0 +1,24 @@
+import Root from './card.svelte';
+import Title from './card-title.svelte';
+import Footer from './card-footer.svelte';
+import Header from './card-header.svelte';
+import Action from './card-action.svelte';
+import Content from './card-content.svelte';
+import Description from './card-description.svelte';
+
+export {
+	Root,
+	Title,
+	Footer,
+	Header,
+	Action,
+	Content,
+	Description,
+	Root as Card,
+	Title as CardTitle,
+	Footer as CardFooter,
+	Header as CardHeader,
+	Action as CardAction,
+	Content as CardContent,
+	Description as CardDescription
+};
