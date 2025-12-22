727ac519910768b67958f8cdf4b71483f2a1d9af
Jay Kerkar <bytewise0405@gmail.com>
2025-12-22 19:59:01 +0530

    refactor: Simplify error handling component by removing unused icons and improving layout

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,33 +1,25 @@
 <script lang="ts">
 	import { page } from '$app/state';
-	import type { Component } from 'svelte';
 	import HomeIcon from '@lucide/svelte/icons/home';
 	import { Button } from '$lib/components/ui/button';
-	import WrenchIcon from '@lucide/svelte/icons/wrench';
-	import SearchXIcon from '@lucide/svelte/icons/search-x';
-	import TriangleAlertIcon from '@lucide/svelte/icons/triangle-alert';
 	import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '$lib/components/ui/card';
 
 	const errorConfig: Record<
 		number,
 		{
-			icon: Component;
 			message: string;
 			description: string;
 		}
 	> = {
 		500: {
-			icon: TriangleAlertIcon,
 			message: 'Internal Server Error',
 			description: 'Something went wrong on our end.'
 		},
 		503: {
-			icon: WrenchIcon,
 			message: 'Service Unavailable',
 			description: 'Service temporarily unavailable.'
 		},
 		404: {
-			icon: SearchXIcon,
 			message: 'Not Found',
 			description: "The page you're looking for doesn't exist."
 		}
@@ -36,7 +28,6 @@
 	const getErrorConfig = (status: number) => {
 		return (
 			errorConfig[status] ?? {
-				icon: TriangleAlertIcon,
 				message: 'Unexpected Error',
 				description: 'An unexpected error occurred.'
 			}
@@ -47,19 +38,15 @@
 </script>
 
 <div class="grid min-h-[calc(100svh-(--spacing(16)))] place-items-center px-6 py-12">
-	<Card class="w-full max-w-lg border-border py-7.5 shadow-sm">
-		<CardHeader class="justify-items-center gap-4 pt-2 text-center">
-			{@const Icon = config.icon}
-			<div class="inline-flex size-18 items-center justify-center rounded-full border bg-muted">
-				<Icon class="size-9 text-foreground" strokeWidth={1.75} />
-			</div>
+	<Card class="w-full max-w-lg border-border py-10 shadow-sm">
+		<CardHeader class="space-y-2 text-center">
+			<CardTitle class="text-7xl font-bold">
+				{page.status}
+			</CardTitle>
 
-			<div class="space-y-1">
-				<CardTitle class="text-6xl font-bold tracking-tighter">{page.status}</CardTitle>
-				<p class="text-xl font-semibold tracking-tight text-foreground">
-					{config.message}
-				</p>
-			</div>
+			<p class="text-2xl font-semibold tracking-tight text-foreground">
+				{config.message}
+			</p>
 		</CardHeader>
 
 		<CardContent class="text-center">
@@ -68,7 +55,7 @@
 			</p>
 		</CardContent>
 
-		<CardFooter class="justify-center">
+		<CardFooter class="justify-center pt-2">
 			<Button href="/" size="lg" class="gap-2">
 				<HomeIcon class="size-4" />
 				Go back home
