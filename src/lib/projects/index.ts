77778bcb936ae9bc1b21165e81e071d94626faa5
Jay Kerkar <bytewise0405@gmail.com>
2026-01-03 13:18:24 +0530

    refactor: Clean up project data structure and remove unused extendedDescriptionPath

--- src/lib/projects/index.ts
+++ src/lib/projects/index.ts
@@ -1,7 +1,53 @@
+import {
+	siZod,
+	siVite,
+	siReact,
+	siPrisma,
+	siFirebase,
+	siSolidity,
+	siEthereum,
+	siNextdotjs,
+	siTypescript,
+	siPostgresql,
+	siCloudinary,
+	siTailwindcss
+} from 'simple-icons';
 import type { Project } from './types';
-import { siVite, siSolidity, siEthereum, siTypescript, siTailwindcss } from 'simple-icons';
 
 export const projects: Project[] = [
+	{
+		featured: true,
+		id: 'vesit-rail',
+		title: 'VESITRail',
+		deployedLink: 'https://vesitrail.vercel.app',
+		githubLink: 'https://github.com/VESITRail/VESITRail',
+		shortDescription: 'Streamlined Railway Concessions with Real-time Tracking',
+		techStack: [
+			siZod,
+			siReact,
+			siPrisma,
+			siFirebase,
+			siNextdotjs,
+			siTypescript,
+			siCloudinary,
+			siPostgresql,
+			siTailwindcss
+		],
+		teamMembers: [
+			{
+				githubUsername: 'Afnankazi'
+			},
+			{
+				githubUsername: 'jaykerkar0405'
+			},
+			{
+				githubUsername: 'Anissh280507'
+			},
+			{
+				githubUsername: 'WhyAsh5114'
+			}
+		]
+	},
 	{
 		id: 'vault-id',
 		featured: false,
@@ -11,7 +57,6 @@ export const projects: Project[] = [
 		githubLink: 'https://github.com/Crew-object-Object/VaultID',
 		shortDescription: 'Portable, wallet-encrypted authentication sessions',
 		techStack: [siVite, siSolidity, siEthereum, siTypescript, siTailwindcss],
-		extendedDescriptionPath: '/src/lib/projects/extended_descriptions/vault-id.md',
 		teamMembers: [
 			{
 				githubUsername: 'jaykerkar0405'
